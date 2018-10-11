ae1ec74e921c4ec5399d1e21e5699ea4e079eb77
Jason Stumbaugh <stumbaughjason@gmail.com>
2018-10-10 19:44:07 -0500

    Add basic react file structure

---
+++ app/javascript/packs/twitterApp/routes.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  BrowserRouter as Router,
+  Route,
+} from 'react-router-dom'
+
+import Home from './components/home';
+
+const App = (props) => (
+  <Router>
+    <div>
+      <Route exact path='/' component={Home} />
+    </div>
+  </Router>
+
+)
+
+export default App;
