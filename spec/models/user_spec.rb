76de5f7902da89ed9d92e9990b4350c82ea25112
Jason Stumbaugh <stumbaughjason@gmail.com>
2018-10-16 21:14:18 -0500

    Create users

---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require "rails_helper"
+
+RSpec.describe User, type: :model do
+  describe "validations" do
+    it { is_expected.to validate_presence_of(:username) }
+    it { is_expected.to validate_presence_of(:password) }
+    it { is_expected.to validate_uniqueness_of(:username) }
+    it { is_expected.to validate_length_of(:password).is_at_least(8) }
+  end
+end
