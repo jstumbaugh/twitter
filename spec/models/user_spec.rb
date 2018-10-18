29559b984da9554a58f6ce3e215ff4655f4b193f
Jason Stumbaugh <stumbaughjason@gmail.com>
2018-10-17 19:12:25 -0500

    Add like associations on users and tweets

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,8 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
+  it { is_expected.to have_many(:likes) }
+
   describe "validations" do
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:password) }
